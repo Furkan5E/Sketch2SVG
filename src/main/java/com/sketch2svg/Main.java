d0b6341aef4a91c99e769bb058f3d559d3610dc9
Furkan5E <furkankeles10@hotmail.com>
2026-07-16 12:17:13 +0100

    feat: implemented CLI argument parser and help interface

--- src/main/java/com/sketch2svg/Main.java
+++ src/main/java/com/sketch2svg/Main.java
@@ -1,14 +1,78 @@
 package com.sketch2svg;
+
 import com.sketch2svg.parser.Sketch;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		var dir = "src/main/resources/";
-		var sketch = new Sketch();
-		// Sketch.render() attempts to read a sketch file and if successful, outputs an SVG file. THe first argument is the input/output directory and the second argument is the base name of the files. For example, if the base name is "foo" it will attempt to read in foo.txt and output foo.svg.
-		sketch.render(dir, "sketch");
-	}
+    public static void main(String[] args) {
+        String inputPath = null;
+        String outputPath = null;
 
+        // parse command line arguments
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "-i", "--input" -> {
+                    if (i + 1 < args.length) {
+                        inputPath = args[++i];
+                    }
+                }
+                case "-o", "--output" -> {
+                    if (i + 1 < args.length) {
+                        outputPath = args[++i];
+                    }
+                }
+                case "-h", "--help" -> {
+                    printHelp();
+                    return;
+                }
+                default -> {
+                    // if a positional argument without flag is provided
+                    if (inputPath == null && !args[i].startsWith("-")) {
+                        inputPath = args[i];
+                    } else if (outputPath == null && !args[i].startsWith("-")) {
+                        outputPath = args[i];
+                    } else {
+                        System.err.println("Unknown argument: " + args[i]);
+                        printHelp();
+                        return;
+                    }
+                }
+            }
+        }
+
+        // default fallback path
+        if (inputPath == null) {
+            inputPath = "src/main/resources/sketch.txt";
+        }
+        if (outputPath == null) {
+            outputPath = inputPath.replaceAll("(?i)\\.txt$", "") + ".svg";
+            if (outputPath.equals(inputPath)) {
+                outputPath = inputPath + ".svg";
+            }
+        }
+
+        System.out.println("Processing: " + inputPath);
+        System.out.println("Destination: " + outputPath);
+
+        Sketch sketch = new Sketch();
+        sketch.fromFile(inputPath);
+        sketch.exportSVG(outputPath);
+
+        System.out.println("Successfully generated SVG: " + outputPath);
+    }
+
+    private static void printHelp() {
+        System.out.println("""
+            Sketch2SVG - Vector Graphics CLI Generator
+            
+            Usage:
+              java -jar sketch2svg.jar [options]
+              java -jar sketch2svg.jar <input.txt> <output.svg>
+            
+            Options:
+              -i, --input <file>     Path to the source sketch .txt file
+              -o, --output <file>    Path for the generated .svg file
+              -h, --help             Display this help message
+            """);
+    }
 }
